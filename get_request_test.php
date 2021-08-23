7f9f92ffaadc9a1e1c6de70317d595712ff6e551
mjviera <55440043+mjviera@users.noreply.github.com>
2021-08-23 16:56:21 +0100

    Update get_request_test.php

--- get_request_test.php
+++ get_request_test.php
@@ -1,14 +1,7 @@
 <?php
 
-    $theVar = array('customInt' => 5);
-   
-    header('Content-Type: application/json');
-    
-    //Encode the variable, and save the encoded string
-    $encoded = json_encode($theVar);
-
-    //Output it
-    echo $encoded;
+    $theVar = "HelloWorld";
+    echo $theVar;
 
 
 ?>
